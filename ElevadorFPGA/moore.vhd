52b5173beabe59192b979aa891a60594a15ff3b0
Lucas Greff Meneses <102193062+greffao@users.noreply.github.com>
2022-11-23 18:03:05 -0300

    Add files via upload

--- ElevadorFPGA/moore.vhd
+++ ElevadorFPGA/moore.vhd
@@ -1,12 +1,22 @@
 ENTITY moore IS
-	PORT(clk						: IN bit; -- clock do sistema
-		  reset					: IN bit; -- reset do programa
-		  andarDesejado		: IN integer RANGE 0 TO 15; -- Andar de destino
-		  andarAtual			: BUFFER integer RANGE 0 TO 15; -- Andar atual
-		  saida					: OUT BIT_VECTOR (1 DOWNTO 0)); -- Saida do motor
-		  /*
-		  A saida diz se o elevador esta subindo, descendo ou parado.
-		  */
+	PORT(
+	     -- clock do sistema
+	     clk						: IN bit;
+		  
+		  -- reset do programa
+		  reset					: IN bit;
+		  
+		  -- Andar de destino
+		  andarDesejado		: IN integer RANGE 0 TO 15;
+		  
+		  -- Andar atual
+		  andarAtual			: BUFFER integer RANGE 0 TO 15;
+		  
+		  -- Saida do motor
+		  saida					: OUT BIT_VECTOR (1 DOWNTO 0));
+		
+		  --A saida diz se o elevador esta subindo, descendo ou parado.
+		  
 END moore;
 
 
@@ -18,35 +28,40 @@ ARCHITECTURE elevador OF moore IS
 		PROCESS(clk, reset, andarAtual)
 		BEGIN
 			IF reset = '1' THEN 
-			/*Manda o elevador pro andar 0 e estado parado
-			sem depender do clock.*/
+			-- Manda o elevador pro andar 0 e estado parado
+			-- sem depender do clock.
+			
 				andarAtual <= 0;
 				estadoAtual <= parado;
 				
 			ELSIF(clk'EVENT AND clk = '1') THEN
-			/*Condicionais que dependem do clock:*/
-				IF(andarDesejado - andarAtual > 0) THEN
-				/*Entrada maior do que 0:
-				- Estado subindo.
-				- Atualiza o andar para o de cima.
-				- Saida subindo*/
+			-- Condicionais que dependem do clock:
+			
+				IF(andarDesejado - andarAtual > 0) THEN		
+				--Entrada maior do que 0:
+				-- - Estado subindo.
+				-- - Atualiza o andar para o de cima.
+				-- - Saida subindo
+				
 					estadoAtual <= subindo;
 					andarAtual <= andarAtual + 1;
 					saida <= "10";
 					
 				ELSIF(andarDesejado - andarAtual < 0) THEN
-				/*Entrada menor do que 0:
-				- Estado descendo.
-				- Atualiza o andar para o de baixo.
-				- Saida descendo*/
+				--Entrada menor do que 0:
+				-- - Estado descendo.
+				-- - Atualiza o andar para o de baixo.
+				-- - Saida descendo
+				
 					estadoAtual <= descendo;
 					andarAtual <= andarAtual - 1;
 					saida <= "01";		
 					
 				ELSE
-				/*Entrada igual a 0:
-				- Estado parado.
-				- Saida parado*/
+				--Entrada igual a 0:
+				-- - Estado parado.
+				-- - Saida parado
+				
 					estadoAtual <= parado;
 					saida <= "00";
 				END IF;
